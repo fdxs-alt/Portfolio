cf2b696fcb28c7ba8842b04592d5f872edaecfc5
Jakub Sukiennik <kuba1207710@gmail.com>
2021-03-24 20:07:08 +0100

    adding new project

--- src/components/MyWork.tsx
+++ src/components/MyWork.tsx
@@ -2,40 +2,40 @@ import React from 'react';
 import data from '../data/data';
 import Project from './Project';
 import {
-    WorkWrapper,
-    ProjectsTitle,
-    ProjectTitleSmall,
+  WorkWrapper,
+  ProjectsTitle,
+  ProjectTitleSmall,
 } from '../style/AboutMe.styles';
 
 export interface IData {
-    description: string;
-    link: string;
-    photo?: undefined;
-    workInProgress?: boolean;
-    deploy?: string;
+  description: string;
+  link: string;
+  photo?: undefined;
+  workInProgress?: boolean;
+  deploy?: string;
 }
 
 const MyWork = () => {
-    return (
-        <WorkWrapper>
-            <ProjectsTitle>My projects</ProjectsTitle>
-            <ProjectTitleSmall>
-                Here are some of my projects. Some of them need to be deyployed,
-                or I'm working on them, but are almost finished.
-            </ProjectTitleSmall>
-            {data.map((p: IData, i: number) => (
-                <Project
-                    key={i}
-                    description={p.description}
-                    link={p.link}
-                    photo={p.photo}
-                    workInProgress={p.workInProgress}
-                    deploy={p.deploy}
-                    order={i + 1}
-                />
-            ))}
-        </WorkWrapper>
-    );
+  return (
+    <WorkWrapper>
+      <ProjectsTitle>My projects</ProjectsTitle>
+      <ProjectTitleSmall>
+        Here are some of my projects. Some of them need to be deyployed, or I'm
+        working on them, but are almost finished.
+      </ProjectTitleSmall>
+      {data.map((p: IData, i: number) => (
+        <Project
+          key={i}
+          description={p.description}
+          link={p.link}
+          photo={p.photo}
+          workInProgress={p.workInProgress}
+          deploy={p.deploy}
+          order={i + 1}
+        />
+      ))}
+    </WorkWrapper>
+  );
 };
 
 export default MyWork;
--- src/data/data.ts
+++ src/data/data.ts
@@ -4,53 +4,62 @@ import Project2 from '../images/scrnli_10_20_2020_11-50-32 AM.png';
 import Project4 from '../images/scrnli_10_20_2020_9-48-51 AM.png';
 import Project5 from '../images/Screenshot.png';
 import Project6 from '../images/Screenshot2.png';
+import Project7 from '../images/project7.png';
 
 const data = [
-    {
-        photo: Project1,
-        description:
-            'Simple app, built with React and Express, with MongoDB, allowing user to shorten their links.',
-        link: 'https://github.com/fdxs-alt/url-shortener',
-        workInProgress: false,
-        deploy: 'https://url-shortener-jsss.herokuapp.com/',
-    },
-    {
-        photo: Project2,
-        description:
-            'Chat app built with React and Redux on frontend, on backend Node.js with Express framework. It allows user to register, log in, creating/deleting rooms, and sending messages via websockets.',
-        link: 'https://github.com/fdxs-alt/Chatty',
-        workInProgress: false,
-        deploy: 'https://chatty.herokuapp.com/',
-    },
-    {
-        photo: Project3,
-        description:
-            'Clone of popular page nofluffjobs.com created with Apollo GraphQl + Typescript + TypeGraphQL, allowing users to menage their company, or sending cvs and storing them in database',
-        link: 'https://github.com/fdxs-alt/nofluffjobs-clone',
-        workInProgress: false,
-    },
-    {
-        photo: Project4,
-        description:
-            'Clone of twitter with React, MobX and Nestjs with TypeORM+PostgreSQL (work in progress). Most complicated project so far. It is supposed to provide all basic Twitter functionality and also sending messages.',
-        link: 'https://github.com/fdxs-alt/twitter-clone',
-        workInProgress: true,
-    },
-    {
-        photo: Project5,
-        link: 'https://github.com/fdxs-alt/Artesania-shop',
-        description:
-            'Landing page build for Artesania company. Created with Next.js and DatoCMS as headless cms. Added animations using react-spring, protected by recaptcha. Seo provided with package Next-seo.',
-        workInProgress: false,
-        deploy: 'https://sklep-artesania.pl/',
-    },
-    {
-        photo: Project6,
-        description:
-            'Application which is using microservice architecture – a gateway pattern, gatway „talks” with other microservices. Pretty simple app, my introduction to learning docker, and docker-compose. More in readme',
-        link: 'https://github.com/fdxs-alt/express-docker',
-        workInProgress: false,
-    },
+  {
+    photo: Project1,
+    description:
+      'Simple app, built with React and Express, with MongoDB, allowing user to shorten their links.',
+    link: 'https://github.com/fdxs-alt/url-shortener',
+    workInProgress: false,
+    deploy: 'https://url-shortener-jsss.herokuapp.com/',
+  },
+  {
+    photo: Project2,
+    description:
+      'Chat app built with React and Redux on frontend, on backend Node.js with Express framework. It allows user to register, log in, creating/deleting rooms, and sending messages via websockets.',
+    link: 'https://github.com/fdxs-alt/Chatty',
+    workInProgress: false,
+    deploy: 'https://chatty.herokuapp.com/',
+  },
+  {
+    photo: Project3,
+    description:
+      'Clone of popular page nofluffjobs.com created with Apollo GraphQl + Typescript + TypeGraphQL, allowing users to menage their company, or sending cvs and storing them in database',
+    link: 'https://github.com/fdxs-alt/nofluffjobs-clone',
+    workInProgress: false,
+  },
+  {
+    photo: Project4,
+    description:
+      'Clone of twitter with React, MobX and Nestjs with TypeORM+PostgreSQL (work in progress). Most complicated project so far. It is supposed to provide all basic Twitter functionality and also sending messages.',
+    link: 'https://github.com/fdxs-alt/twitter-clone',
+    workInProgress: true,
+  },
+  {
+    photo: Project5,
+    link: 'https://github.com/fdxs-alt/Artesania-shop',
+    description:
+      'Landing page build for Artesania company. Created with Next.js and DatoCMS as headless cms. Added animations using react-spring, protected by recaptcha. Seo provided with package Next-seo.',
+    workInProgress: false,
+    deploy: 'https://sklep-artesania.pl/',
+  },
+  {
+    photo: Project6,
+    description:
+      'Application which is using microservice architecture – a gateway pattern, gatway „talks” with other microservices. Pretty simple app, my introduction to learning docker, and docker-compose. More in readme',
+    link: 'https://github.com/fdxs-alt/express-docker',
+    workInProgress: false,
+  },
+  {
+    photo: Project7,
+    description:
+      'Nextjs app allowing admin to sell games. Using DatoCMS as headless CMS. Paymenets provided by stripe. Full cart functionality. Data about orders stored in MongoDB.',
+    deploy: 'https://next-shop-stripe.vercel.app/',
+    link: 'https://github.com/fdxs-alt/nextjs-shop',
+    workInProgress: false,
+  },
 ];
 
 export default data;
